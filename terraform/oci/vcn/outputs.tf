5875a1fb21d11c619742680a05a15c6c0101f4ba
Willian Pereira Almeida <willianpereiraalmeida@hotmail.com>
2024-01-17 20:53:44 +0000

    add oke node config

--- terraform/oci/vcn/outputs.tf
+++ terraform/oci/vcn/outputs.tf
@@ -1,3 +1,15 @@
 output "vcn_id" {
   value = oci_core_vcn.guiadodevops.id
 }
+
+output "service_lb_subnet_id" {
+  value = oci_core_subnet.service_lb.id
+}
+
+output "node_subnet_id" {
+  value = oci_core_subnet.node.id
+}
+
+output "api_endpoint_subnet_id" {
+  value = oci_core_subnet.api_endpoint.id
+}
