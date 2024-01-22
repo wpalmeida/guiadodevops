9ba1668a6b941213ae7b47e65345a2a36adcc844
Willian Pereira Almeida <willianpereiraalmeida@hotmail.com>
2024-01-22 22:52:25 +0000

    add config for runner

---
+++ terraform/oci/instance/runners/backend.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "guiadodevops"
+    storage_account_name = "guiadodevops"
+    container_name       = "terraform"
+    key                  = "runners/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "vcn" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "guiadodevops"
+    storage_account_name = "guiadodevops"
+    container_name       = "terraform"
+    key                  = "vcn/terraform.tfstate"
+  }
+}
