dc65e744a221f54b3d4297e4a54a45884a268bdf
Willian Pereira Almeida <willianpereiraalmeida@hotmail.com>
2024-01-28 15:02:33 +0000

    apply fmt on terraform new compartments

--- terraform/oci/compartment/nonprod/main.tf
+++ terraform/oci/compartment/nonprod/main.tf
@@ -2,6 +2,6 @@ resource "oci_identity_compartment" "nonprod_compartment" {
   compartment_id = var.compartment_id
   description    = var.compartment_description
   name           = var.compartment_name
-  enable_delete        = true
+  enable_delete  = true
   freeform_tags  = { "env" = "nonprod" }
 }
--- terraform/oci/compartment/shared/main.tf
+++ terraform/oci/compartment/shared/main.tf
@@ -2,6 +2,6 @@ resource "oci_identity_compartment" "shared_compartment" {
   compartment_id = var.compartment_id
   description    = var.compartment_description
   name           = var.compartment_name
-  enable_delete        = true
+  enable_delete  = true
   freeform_tags  = { "env" = "shared" }
 }
